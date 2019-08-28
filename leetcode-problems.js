054e3e96cf5379d4ed4a9948f5d62ebe7d1f7c9c
Nian-Hu <nianhu96@gmail.com>
2019-08-28 14:05:01 -0400

    Start longest substring

--- leetcode-problems.js
+++ leetcode-problems.js
@@ -29,14 +29,25 @@ function twoSum(arr, target) {
 
 console.log(twoSum([2, 7, 11, 15], 9))
 
-// You are given two non - empty linked lists representing two non - negative integers.
-// The digits are stored in reverse order and each of their nodes contain a 
-// single digit.Add the two numbers and return it as a linked list.
+// Given a string, find the length of the longest substring without repeating characters.
 
-// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//   Example 1:
 
-//   Example:
+// Input: "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
 
-// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-// Output: 7 -> 0 -> 8
-// Explanation: 342 + 465 = 807.
+// Input: "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+function longestSubstring(str) {
+
+}

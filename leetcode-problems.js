0248460bf59c2b8501dd512c1d6e83cfc2c28e81
Nian-Hu <nianhu96@gmail.com>
2019-08-28 16:11:42 -0400

    Start reverse linked list

--- leetcode-problems.js
+++ leetcode-problems.js
@@ -79,3 +79,15 @@ function longestSubstring(str) {
 // longestSubstring("abcabcbb")
 
 console.log(longestSubstring("pwwkew"))
+
+// Reverse a singly linked list.
+
+//   Example:
+
+// Input: 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+// Output: 5 -> 4 -> 3 -> 2 -> 1 -> NULL
+// Follow up:
+
+// A linked list can be reversed either iteratively or recursively.Could you implement both?
+
+

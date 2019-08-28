8a3f851cde16cfb4cfcfa836c160f4aefb8c237a
Nian-Hu <nianhu96@gmail.com>
2019-08-28 13:58:18 -0400

    Solve two sum with nested loop

--- leetcode-problems.js
+++ leetcode-problems.js
@@ -12,7 +12,19 @@
 // return [0, 1].
 
 function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    let num1 = arr[i];
 
+    for (let j = i + 1; j < arr.length; j++) {
+      let num2 = arr[j];
+
+      if (num1 + num2 === target) {
+        return [i, j];
+      }
+    }
+  }
+
+  return null;
 }
 
 console.log(twoSum([2, 7, 11, 15], 9))

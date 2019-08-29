833333d2971618770e64020841025c4452221b3a
Nian-Hu <nianhu96@gmail.com>
2019-08-28 22:32:05 -0400

    Start subsets

--- leetcode-problems.js
+++ leetcode-problems.js
@@ -140,3 +140,21 @@ function permutations(array) {
 }
 
 console.log(permutations([1, 2, 3]))
+
+// Returns all subsets of an array
+// do this recursively
+
+//     Example:
+
+// Input: nums = [1, 2, 3]
+// Output:
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1, 2, 3],
+//   [1, 3],
+//   [2, 3],
+//   [1, 2],
+//   []
+// ]

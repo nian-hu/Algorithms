fd9d1c4ffb5f6151ee57c1c11212955da8a6d0b4
Nian-Hu <nianhu96@gmail.com>
2019-09-03 13:16:36 -0400

    Write remove tail for linked list class

--- linked_list_project/lib/linked_list.js
+++ linked_list_project/lib/linked_list.js
@@ -57,7 +57,21 @@ class LinkedList {
       if (!this.head) return undefined;
       let current = this.head;
       let newTail = current;
-      
+
+      while (current.next) {
+        newTail = current;
+        current = current.next;
+      }
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+
+      if (this.length === 0) {
+        this.head = null;
+        this.tail = null;
+      }
+
+      return current;
     }
 
     // TODO: Implement the addToHead method here

1b9c2d63374e30be4c537063c3ad89ef533c59ea
Nian-Hu <nianhu96@gmail.com>
2019-09-03 10:21:43 -0400

    Start longest common prefix

--- leetcode-problems.js
+++ leetcode-problems.js
@@ -175,3 +175,18 @@ function subsets(array) {
 }
 
 console.log(subsets([1, 2, 3]))
+
+// Write a function to find the longest common prefix string amongst
+//  an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+//   Example 1:
+
+// Input: ["flower", "flow", "flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: ["dog", "racecar", "car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.

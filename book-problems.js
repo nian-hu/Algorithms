8540bd46cdd18ccbb27580bac9c337aefd49e66a
Nian-Hu <nianhu96@gmail.com>
2019-09-07 12:21:19 -0400

    Solve check perms problem

--- book-problems.js
+++ book-problems.js
@@ -1,4 +1,3 @@
-// isUnique
 // Determine if a string has all unique characters
 
 function isUnique(str) {
@@ -15,3 +14,28 @@ function isUnique(str) {
 console.log(isUnique('abcdef')) // true
 console.log(isUnique('abcdefa'))// false
 console.log(isUnique('aaaa'))   // false 
+
+// Decide if one string is a permutation of another
+
+function checkPerms(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  let set = new Set();
+  for (let i = 0; i < str1.length; i++) {
+    set.add(str2[i]);
+  }
+
+  while (str1.length > 0) {
+    if (set.has(str1[0])) {
+      str1 = str1.slice(1);
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(checkPerms('dog', 'god')) // true
+console.log(checkPerms('dog', 'dod')) // false
+console.log(checkPerms('dog', 'dogd')) // false

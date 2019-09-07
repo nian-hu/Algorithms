3253b0f27b84e194d875b938b54a6c5e809a854c
Nian-Hu <nianhu96@gmail.com>
2019-09-07 12:09:36 -0400

    Start is unique problem

---
+++ book-problems.js
@@ -0,0 +1,10 @@
+// isUnique
+// Determine if a string has all unique characters
+
+function isUnique(str) {
+
+}
+
+console.log(isUnique('abcdef')) // true
+console.log(isUnique('abcdefa'))// false
+console.log(isUnique('aaaa'))   // false 

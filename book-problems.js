ea41910da72ffa2ec9deefbb2b7e405bab2e14a7
Nian-Hu <nianhu96@gmail.com>
2019-09-07 14:54:16 -0400

    Solve rotate matrix another way

--- book-problems.js
+++ book-problems.js
@@ -227,7 +227,33 @@ function rotateMatrix(matrix) {
 // console.log(rotateMatrix([[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, 16]]))
 
 function rotateMatrix2(matrix) {
-  
+  let length = matrix.length;
+
+  // swap all the rows
+  // so now it looks like this
+  // [[13, 14, 15, 16],
+  //   [9, 10, 11, 12],
+  //   [5, 6, 7, 8],
+  //   [1, 2, 3, 4]]
+  for (let i = 0; i < length / 2; i++) {
+    let j = length - 1 - i;
+    [matrix[i], matrix[j]] = [matrix[j], matrix[i]]
+  }
+
+  //swap diagonals
+  // so now it looks like this
+  // [[13, 9, 5, 1],
+  //   [14, 10, 6, 2],
+  //   [15, 11, 7, 3],
+  //   [16, 12, 8, 4]]
+
+  for (let i = 0; i < length - 1; i++) {
+    for (let j = i + 1; j < length; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+    }
+  }
+
+  return matrix;
 }
 
 console.log(rotateMatrix2([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]))

bb011954ae2710c12f15281b199e5acb4adbd539
Nian-Hu <nianhu96@gmail.com>
2019-09-07 12:11:27 -0400

    Solve is unique problem

--- book-problems.js
+++ book-problems.js
@@ -2,7 +2,14 @@
 // Determine if a string has all unique characters
 
 function isUnique(str) {
+  for (let i = 0; i < str.length; i++) {
+    let first = str[0];
+    let rest = str.slice(1);
 
+    if (rest.includes(first)) return false;
+  }
+
+  return true;
 }
 
 console.log(isUnique('abcdef')) // true
